0f0ff18e55a5b2c0b4f97e683dd7d1f24d5cb050
pc <pc@pcs-MacBook-Pro.local>
2026-01-06 21:05:41 +0100

    sefwef

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- Resize window using <ctrl> arrow keys
+vim.keymap.set("n", "<M-h>", "<C-w><", { desc = "Resize left" })
+vim.keymap.set("n", "<M-l>", "<C-w>>", { desc = "Resize right" })
+vim.keymap.set("n", "<M-j>", "<C-w>-", { desc = "Resize down" })
+vim.keymap.set("n", "<M-k>", "<C-w>+", { desc = "Resize up" })
